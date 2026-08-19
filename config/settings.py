feaf2e04c6baafe8ba19ee680aaf30fa2581efda
Ikolawole2020 <inioluwaolabola@gmail.com>
2026-08-19 17:37:16 +0100

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -34,7 +34,6 @@ INSTALLED_APPS = [
 
     # Third party
     'rest_framework',
-    'corsheaders',
 
     # Local apps
     'users.apps.UsersConfig',
@@ -114,7 +113,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:3000",
     "http://taskifyer-snowy.vercel.app"
 ]
-
+CORS_ALLOW_ALL_ORIGIN = True
 CORS_ALLOW_CREDENTIALS = True
 
 # REST Framework
